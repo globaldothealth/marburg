25ce110f6d0333f5f8325098b107064ae4c36376
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2023-03-30 17:10:12 +0100

    Replace ebola with marburg

--- scripts/ingestion/run.py
+++ scripts/ingestion/run.py
@@ -17,7 +17,7 @@ S3 = boto3.resource("s3")
 LOCALSTACK_URL = os.environ.get("LOCALSTACK_URL")
 
 if LOCALSTACK_URL:
-	S3 = boto3.resource("s3", endpoint_url=LOCALSTACK_URL)
+    S3 = boto3.resource("s3", endpoint_url=LOCALSTACK_URL)
 
 S3_BUCKET = os.environ.get("S3_BUCKET")
 S3_FOLDER = os.environ.get("S3_FOLDER")
@@ -34,73 +34,73 @@ TODAY = datetime.today()
 
 
 def setup_logger():
-	h = logging.StreamHandler(sys.stdout)
-	rootLogger = logging.getLogger()
-	rootLogger.addHandler(h)
-	rootLogger.setLevel(logging.DEBUG)
+    h = logging.StreamHandler(sys.stdout)
+    rootLogger = logging.getLogger()
+    rootLogger.addHandler(h)
+    rootLogger.setLevel(logging.DEBUG)
 
 
 def get_data():
-	logging.info("Getting data from Google Sheets")
-	client = pygsheets.authorize(service_account_env_var="GOOGLE_CREDENTIALS")
-	spreadsheet = client.open_by_key(DOCUMENT_ID)
+    logging.info("Getting data from Google Sheets")
+    client = pygsheets.authorize(service_account_env_var="GOOGLE_CREDENTIALS")
+    spreadsheet = client.open_by_key(DOCUMENT_ID)
 
-	return spreadsheet[0].get_all_records()
+    return spreadsheet[0].get_all_records()
 
 
 def clean_data(data):
-	logging.info("Cleaning data")
-	for c in data:
-		for field in PRIVATE_FIELDS:
-			if field in c:
-				c.pop(field)
-	return data
+    logging.info("Cleaning data")
+    for c in data:
+        for field in PRIVATE_FIELDS:
+            if field in c:
+                c.pop(field)
+    return data
 
 
 def format_data(data):
-	logging.info("Formatting data")
-	csv_data = ""
-	column_names = data[0].keys()
-	for name in column_names:
-		csv_data += f"{name},"
-	csv_data += "\n"
-	for row in data:
-		for val in row.values():
-			csv_data += f"{str(val).replace(',', ';')},"
-		csv_data += "\n"
-	return csv_data
+    logging.info("Formatting data")
+    csv_data = ""
+    column_names = data[0].keys()
+    for name in column_names:
+        csv_data += f"{name},"
+    csv_data += "\n"
+    for row in data:
+        for val in row.values():
+            csv_data += f"{str(val).replace(',', ';')},"
+        csv_data += "\n"
+    return csv_data
 
 
 def store_data(csv_data):
-	logging.info("Uploading data to S3")
-	try:
-		S3.Object(S3_BUCKET, f"{S3_FOLDER}/{TODAY}.csv").put(Body=csv_data)
-		S3.Object(S3_BUCKET, f"latest.csv").put(Body=csv_data)
-	except Exception as exc:
-		logging.exception(f"An exception occurred while trying to upload files")
-		raise
+    logging.info("Uploading data to S3")
+    try:
+        S3.Object(S3_BUCKET, f"{S3_FOLDER}/{TODAY}.csv").put(Body=csv_data)
+        S3.Object(S3_BUCKET, f"latest.csv").put(Body=csv_data)
+    except Exception as exc:
+        logging.exception(f"An exception occurred while trying to upload files")
+        raise
 
 
 def data_to_db(data):
-	logging.info("Adding data to the database")
-	try:
-		client = MongoClient(DB_CONNECTION)
-		database = client[DATABASE_NAME]
-		for entry in data:
-			find = {"ID": entry["ID"]}
-			update = {"$set": entry}
-			database[GH_COLLECTION].update_one(find, update, upsert=True)
-	except PyMongoError:
-		logging.exception("An error occurred while trying to insert data")
-		raise
+    logging.info("Adding data to the database")
+    try:
+        client = MongoClient(DB_CONNECTION)
+        database = client[DATABASE_NAME]
+        for entry in data:
+            find = {"ID": entry["ID"]}
+            update = {"$set": entry}
+            database[GH_COLLECTION].update_one(find, update, upsert=True)
+    except PyMongoError:
+        logging.exception("An error occurred while trying to insert data")
+        raise
 
 
 if __name__ == "__main__":
-	setup_logger()
-	logging.info("Starting Ebola data ingestion")
-	data = get_data()
-	data = clean_data(data)
-	csv_data = format_data(data)
-	store_data(csv_data)
-	data_to_db(data)
-	logging.info("Work complete")
+    setup_logger()
+    logging.info("Starting Marburg 2023 data ingestion")
+    data = get_data()
+    data = clean_data(data)
+    csv_data = format_data(data)
+    store_data(csv_data)
+    data_to_db(data)
+    logging.info("Work complete")
--- scripts/ingestion/test_run.py
+++ scripts/ingestion/test_run.py
@@ -4,16 +4,25 @@ import boto3
 from pymongo import MongoClient
 import pytest
 
-from run import (DB_CONNECTION, DATABASE_NAME, GH_COLLECTION, S3_BUCKET, LOCALSTACK_URL,
-	clean_data, format_data, data_to_db, store_data)
+from run import (
+    DB_CONNECTION,
+    DATABASE_NAME,
+    GH_COLLECTION,
+    S3_BUCKET,
+    LOCALSTACK_URL,
+    clean_data,
+    format_data,
+    data_to_db,
+    store_data,
+)
 
 CASE = {
     "ID": 1,
     "Date_confirmation": "2021-05-12",
     "Curator_initials": "ZZ",
     "Notes": "example note",
-    "Country": "Uganda",
-    "Status": "confirmed"
+    "Country": "Equatorial Guinea",
+    "Status": "confirmed",
 }
 
 CLEAN_CASE = CASE.copy()
@@ -23,20 +32,20 @@ CLEAN_DATA = [CLEAN_CASE]
 
 CSV_DATA = """
 ID,Date_confirmation,Country,Status,
-1,2021-05-12,Uganda,confirmed,
+1,2021-05-12,Equatorial Guinea,confirmed,
 """
 
 
 def get_contents(file_name: str) -> str:
-	s3 = boto3.resource("s3", endpoint_url=LOCALSTACK_URL)
-	obj = s3.Object(S3_BUCKET, file_name)
-	return obj.get()["Body"].read().decode("utf-8")
+    s3 = boto3.resource("s3", endpoint_url=LOCALSTACK_URL)
+    obj = s3.Object(S3_BUCKET, file_name)
+    return obj.get()["Body"].read().decode("utf-8")
 
 
 def get_db_records(collection: str) -> list[dict]:
-	db = MongoClient(DB_CONNECTION)[DATABASE_NAME][collection]
-	cursor = db.find({})
-	return [record for record in cursor]
+    db = MongoClient(DB_CONNECTION)[DATABASE_NAME][collection]
+    cursor = db.find({})
+    return [record for record in cursor]
 
 
 def test_clean_data():
@@ -44,21 +53,24 @@ def test_clean_data():
 
 
 def test_format_data():
-	assert format_data(CLEAN_DATA).strip("\n") == CSV_DATA.strip("\n")
+    assert format_data(CLEAN_DATA).strip("\n") == CSV_DATA.strip("\n")
 
 
-@pytest.mark.skipif(not os.environ.get("DOCKERIZED", False),
-						reason="Running e2e tests outside of mock environment disabled")
+@pytest.mark.skipif(
+    not os.environ.get("DOCKERIZED", False),
+    reason="Running e2e tests outside of mock environment disabled",
+)
 def test_store_data():
-	store_data(CSV_DATA)
-	assert get_contents("latest.csv") == CSV_DATA
+    store_data(CSV_DATA)
+    assert get_contents("latest.csv") == CSV_DATA
 
 
-@pytest.mark.skipif(not os.environ.get("DOCKERIZED", False),
-						reason="Running e2e tests outside of mock environment disabled")
+@pytest.mark.skipif(
+    not os.environ.get("DOCKERIZED", False),
+    reason="Running e2e tests outside of mock environment disabled",
+)
 def test_data_to_db():
-	data_to_db(CLEAN_DATA)
-	db_records = get_db_records(GH_COLLECTION)
-	del db_records[0]["_id"]
-	assert db_records == CLEAN_DATA
-
+    data_to_db(CLEAN_DATA)
+    db_records = get_db_records(GH_COLLECTION)
+    del db_records[0]["_id"]
+    assert db_records == CLEAN_DATA
